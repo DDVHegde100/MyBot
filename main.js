c235e57b1988c3e0d906a7608f873989aa2c9d55
DDVHegde100 <69629876+DDVHegde100@users.noreply.github.com>
2021-02-16 17:04:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const Discord=require('discord.js');
+const cient=new Discord.Client();
+client.once('ready', ()=>{
+   console.log('INDIAN GUYYY'); 
+})
+client.login('ODEwMzA1NTU5MTMxNTg2NjAx.YCht3A.A_bEcOTDRACZGBoEURrsrxImD5M');
